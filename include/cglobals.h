87976544e816438d4dd5266f7415761a868742fe
vs <vadim.sanzharov@graphics.cs.msu.ru>
2023-05-11 14:49:01 +0300

    move math funcs to litemath

--- include/cglobals.h
+++ include/cglobals.h
@@ -162,15 +162,6 @@ static inline float3 OffsRayPos(const float3 a_hitPos, const float3 a_surfaceNor
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static inline float3 mul3x3(float4x4 m, float3 v)
-{ 
-  return to_float3(m*to_float4(v, 0.0f));
-}
-
-static inline float3 mul4x3(float4x4 m, float3 v)
-{
-  return to_float3(m*to_float4(v, 1.0f));
-}
 
 static inline void transform_ray3f(float4x4 a_mWorldViewInv, float3* ray_pos, float3* ray_dir) 
 {
@@ -229,25 +220,6 @@ static inline MisData makeInitialMisData()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static inline float4x4 perspectiveMatrix(float fovy, float aspect, float zNear, float zFar)
-{
-  const float ymax = zNear * tanf(fovy * 3.14159265358979323846f / 360.0f);
-  const float xmax = ymax * aspect;
-  const float left = -xmax;
-  const float right = +xmax;
-  const float bottom = -ymax;
-  const float top = +ymax;
-  const float temp = 2.0f * zNear;
-  const float temp2 = right - left;
-  const float temp3 = top - bottom;
-  const float temp4 = zFar - zNear;
-  float4x4 res;
-  res.m_col[0] = float4{ temp / temp2, 0.0f, 0.0f, 0.0f };
-  res.m_col[1] = float4{ 0.0f, temp / temp3, 0.0f, 0.0f };
-  res.m_col[2] = float4{ (right + left) / temp2,  (top + bottom) / temp3, (-zFar - zNear) / temp4, -1.0 };
-  res.m_col[3] = float4{ 0.0f, 0.0f, (-temp * zFar) / temp4, 0.0f };
-  return res;
-}
 
 static inline float2 mulRows2x4(const float4 row0, const float4 row1, float2 v)
 {
