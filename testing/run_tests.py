af05b800948553154e12ad9d02d37a88f4c8f05d
Vladimir Frolov <vova@frolov.pp.ru>
2023-07-11 13:12:23 +0300

    update testing

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -10,7 +10,7 @@ from colorama import Fore
 
 TEST_CPU             = False
 PATH_TO_HYDRA2_TESTS = "/home/frol/PROG/HydraRepos/HydraAPI-tests"
-PATH_TO_HYDRA3_SCENS = "/home/frol/PROG/gitflic/comparisonrender"
+PATH_TO_HYDRA3_SCENS = "/home/frol/PROG/HydraRepos/comparisonrender"
 
 ############################################################################################################
 ############################################################################################################
@@ -70,7 +70,7 @@ class REQ_H2(REQ):
       full = PATH_TO_HYDRA2_TESTS + "/tests_f/" + test_name + "/statex_00001.xml"
       devices = ["gpu"] if not TEST_CPU else ["gpu", "cpu"]
       for dev_type in devices:
-        Log().info("  rendering scene: '{0}', dev_type='{1}', dev_id = '{2}'".format(test_name, dev_type, gpu_id))
+        Log().info("  rendering scene: '{0}', dev_type='{1}', scene = '{2}'".format(test_name, dev_type, full))
         for inregrator in req.integs:
           outp = PATH_TO_HYDRA2_TESTS + "/tests_images/" + test_name + "/z_" + dev_type + inregrator + ".bmp"
           args = ["./cmake-build-release/hydra", "-in", full, "-out", outp, "-integrator", inregrator, "-spp-naive-mul", str(req.naivem), "-gamma", "2.2"]
@@ -92,8 +92,8 @@ class REQ_H2(REQ):
           if PSNR < 35.0: (color,message) = (Fore.RED, "[FAILED]") 
           Log().print_colored_text("  {}: PSNR({}, CPU/GPU) = {:.2f}".format(message,alignIntegratorName(inregrator),PSNR), color = color) 
 
-class REQ_H2X(REQ):
-  def __init__(self, name, scn_path, ref_path, imsize = (1024,1024), inregrators = ["naivept","shadowpt","mispt"], naivemul = 4):
+class REQ_HX(REQ):
+  def __init__(self, name, scn_path, ref_path, imsize = [(1024,1024)], inregrators = ["naivept","shadowpt","mispt"], naivemul = 4):
     self.name   = name
     self.scn_path = scn_path
     self.ref_path = ref_path
@@ -102,64 +102,47 @@ class REQ_H2X(REQ):
     self.naivem = naivemul
 
   def test(req, gpu_id=0):
-    for (scnp, imgp, id) in zip(req.scn_path, req.ref_path, range(0,len(req.scn_path))):
+    for (scnp, imgp, id, imsize2) in zip(req.scn_path, req.ref_path, range(0,len(req.scn_path)), req.imsize):
       image_ref   = cv2.imread(imgp, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
       scene_path  = scnp
       folder_path = os.path.dirname(imgp)
       devices   = ["gpu"] if not TEST_CPU else ["gpu", "cpu"]
       test_name = req.name
       for dev_type in devices:
-        Log().info("  rendering scene: '{0}', dev_type='{1}', dev_id = '{2}'".format(test_name, dev_type, gpu_id))
+        Log().info("  rendering scene: '{0}', dev_type='{1}', scene = '{2}'".format(test_name, dev_type, scene_path))
         for inregrator in req.integs:
           outp = folder_path + "/y" + str(id) + "_" + dev_type + inregrator + ".bmp"
           args = ["./cmake-build-release/hydra", "-in", scene_path, "-out", outp, "-integrator", inregrator, "-spp-naive-mul", str(req.naivem), "-gamma", "2.2"]
           args = args + ["-gpu_id", str(gpu_id)]  # for single launch samples
-          args = args + ["-width", str(req.imsize[0]), "-height", str(req.imsize[1])]
-          args = args + ["--" + dev_type]
-          #print(args)
-          req.run(test_name, args, image_ref, outp, inregrator)
-
-class REQ_H3(REQ):
-  def __init__(self, name, scn_path, ref_path, imsize = (1024,1024), inregrators = ["naivept","shadowpt","mispt"], naivemul = 4):
-    self.name   = name
-    self.scn_path = scn_path
-    self.ref_path = ref_path
-    self.imsize = imsize
-    self.integs = inregrators
-    self.naivem = naivemul
-
-  def test(req, gpu_id=0):
-    for (scnp, imgp, id) in zip(req.scn_path, req.ref_path, range(0,len(req.scn_path))):
-      image_ref  = cv2.imread(PATH_TO_HYDRA3_SCENS + "/Report/Images/" + imgp, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
-      scene_path = PATH_TO_HYDRA3_SCENS + "/" + scnp
-      devices   = ["gpu"] if not TEST_CPU else ["gpu", "cpu"]
-      test_name = req.name
-      for dev_type in devices:
-        Log().info("  rendering scene: '{0}', dev_type='{1}', dev_id = '{2}'".format(test_name, dev_type, gpu_id))
-        for inregrator in req.integs:
-          outp = PATH_TO_HYDRA3_SCENS + "/Report/Images/" + test_name  + "/y" + str(id) + "_" + dev_type + inregrator + ".bmp"
-          args = ["./cmake-build-release/hydra", "-in", scene_path, "-out", outp, "-integrator", inregrator, "-spp-naive-mul", str(req.naivem), "-gamma", "2.2"]
-          args = args + ["-gpu_id", str(gpu_id)]  # for single launch samples
-          args = args + ["-width", str(req.imsize[0]), "-height", str(req.imsize[1])]
+          args = args + ["-width", str(imsize2[0]), "-height", str(imsize2[1])]
           args = args + ["--" + dev_type]
           #print(args)
           req.run(test_name, args, image_ref, outp, inregrator)
   
 reqs = []
 
-reqs.append( REQ_H2("mat_lambert", ["test_101"], naivemul = 16) )
+reqs.append( REQ_HX("mat_lambert", [PATH_TO_HYDRA2_TESTS + "/tests_f/test_101/statex_00001.xml",  
+                                    PATH_TO_HYDRA3_SCENS + "/Lambert/Lambert_cornell_hydra.xml"],
+                                   [PATH_TO_HYDRA2_TESTS + "/tests_images/test_101/w_ref.png", 
+                                    PATH_TO_HYDRA3_SCENS + "/Report/Images/Lambert/Lambert_cornell_mitsuba.png"], 
+                                    imsize = [(512,512), (1024,1024)], naivemul = 16))
+
+reqs.append( REQ_HX("mat_emission", [PATH_TO_HYDRA2_TESTS + "/tests_f/test_123/statex_00001.xml",  
+                                     PATH_TO_HYDRA2_TESTS + "/tests_f/test_123/statex_00002.xml",
+                                     PATH_TO_HYDRA2_TESTS + "/tests_f/test_123/statex_00003.xml"],
+                                    [PATH_TO_HYDRA2_TESTS + "/tests_images/test_123/w_ref.png", 
+                                     PATH_TO_HYDRA2_TESTS + "/tests_images/test_123/w_ref2.png",
+                                     PATH_TO_HYDRA2_TESTS + "/tests_images/test_123/w_ref3.png"], 
+                                     imsize = [(512,512), (512,512), (512,512)],
+                                     naivemul = 16, inregrators = ["naivept","mispt"]))
+
 reqs.append( REQ_H2("mat_mirror",  ["test_102"], inregrators = ["naivept","mispt"]) )
 reqs.append( REQ_H2("mat_lambert_texture", ["test_103"]) )
 
-reqs.append( REQ_H2X("mat_emission", [PATH_TO_HYDRA2_TESTS + "/tests_f/test_123/statex_00001.xml",  
-                                      PATH_TO_HYDRA2_TESTS + "/tests_f/test_123/statex_00002.xml",
-                                      PATH_TO_HYDRA2_TESTS + "/tests_f/test_123/statex_00003.xml"],
-                                     [PATH_TO_HYDRA2_TESTS + "/tests_images/test_123/w_ref.png", 
-                                      PATH_TO_HYDRA2_TESTS + "/tests_images/test_123/w_ref2.png",
-                                      PATH_TO_HYDRA2_TESTS + "/tests_images/test_123/w_ref3.png"], naivemul = 16, imsize = (512,512), inregrators = ["naivept","mispt"]))
-
-reqs.append( REQ_H3("SmoothPlastic", ["smooth_plastic/SmoothPlastic_sphere_hydra.xml",  "smooth_plastic/SmoothPlastic_cornell_hydra.xml"],
-                                     ["SmoothPlastic/SmoothPlastic_sphere_mitsuba.png", "SmoothPlastic/SmoothPlastic_cornell_mitsuba.png"], naivemul = 16))
+reqs.append( REQ_HX("mat_smooth_plastic", [PATH_TO_HYDRA3_SCENS + "/smooth_plastic/SmoothPlastic_sphere_hydra.xml",  
+                                           PATH_TO_HYDRA3_SCENS + "/smooth_plastic/SmoothPlastic_cornell_hydra.xml"],
+                                          [PATH_TO_HYDRA3_SCENS + "/Report/Images/SmoothPlastic/SmoothPlastic_sphere_mitsuba.png", 
+                                           PATH_TO_HYDRA3_SCENS + "/Report/Images/SmoothPlastic/SmoothPlastic_cornell_mitsuba.png"], naivemul = 16))
 
 Log().set_workdir(".")
 Log().info("PATH_TO_TESTS = {}".format(PATH_TO_HYDRA2_TESTS))
