3c892eb76b112dd50408aa13ed737b6af5ede55c
Vladimir Frolov <vladimir.frolov@graphics.cs.msu.ru>
2023-10-02 16:03:43 +0300

    forget file

---
+++ include/Integrator_generated_ubo.h
@@ -0,0 +1,72 @@
+#ifndef Integrator_UBO_H
+#define Integrator_UBO_H
+
+#ifndef GLSL
+#define LAYOUT_STD140
+#include "LiteMath.h"
+using LiteMath::uint;
+typedef LiteMath::float4x4 mat4;
+typedef LiteMath::float2   vec2;
+typedef LiteMath::float3   vec3;
+typedef LiteMath::float4   vec4;
+typedef LiteMath::int2     ivec2;
+typedef LiteMath::int3     ivec3;
+typedef LiteMath::int4     ivec4;
+typedef LiteMath::uint2    uvec2;
+typedef LiteMath::uint3    uvec3;
+typedef LiteMath::uint4    uvec4;
+#else
+#define M_PI          3.14159265358979323846f
+#define M_TWOPI       6.28318530717958647692f
+#define INV_PI        0.31830988618379067154f
+#define INV_TWOPI     0.15915494309189533577f
+#endif
+
+struct Integrator_Generated_UBO_Data
+{
+  mat4 m_projInv; 
+  mat4 m_worldViewInv; 
+  vec4 m_envColor; 
+  uint m_intergatorType; 
+  uint m_traceDepth; 
+  int m_winHeight; 
+  int m_winWidth; 
+  uint m_allRemapListsOffsets_capacity; 
+  uint m_allRemapListsOffsets_size; 
+  uint m_allRemapLists_capacity; 
+  uint m_allRemapLists_size; 
+  uint m_instIdToLightInstId_capacity; 
+  uint m_instIdToLightInstId_size; 
+  uint m_lights_capacity; 
+  uint m_lights_size; 
+  uint m_matIdByPrimId_capacity; 
+  uint m_matIdByPrimId_size; 
+  uint m_matIdOffsets_capacity; 
+  uint m_matIdOffsets_size; 
+  uint m_materials_capacity; 
+  uint m_materials_size; 
+  uint m_normMatrices_capacity; 
+  uint m_normMatrices_size; 
+  uint m_pAccelStruct_capacity; 
+  uint m_pAccelStruct_size; 
+  uint m_packedXY_capacity; 
+  uint m_packedXY_size; 
+  uint m_randomGens_capacity; 
+  uint m_randomGens_size; 
+  uint m_remapInst_capacity; 
+  uint m_remapInst_size; 
+  uint m_textures_capacity; 
+  uint m_textures_size; 
+  uint m_triIndices_capacity; 
+  uint m_triIndices_size; 
+  uint m_vNorm4f_capacity; 
+  uint m_vNorm4f_size; 
+  uint m_vTexc2f_capacity; 
+  uint m_vTexc2f_size; 
+  uint m_vertOffset_capacity; 
+  uint m_vertOffset_size; 
+  uint dummy_last;
+};
+
+#endif
+
