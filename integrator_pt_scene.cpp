bad683983d36fe82592897833a64a6f7e3ec7c17
vs <sanzharovvv@my.msu.ru>
2024-02-29 14:12:34 +0300

    upd. CrossRT

--- integrator_pt_scene.cpp
+++ integrator_pt_scene.cpp
@@ -635,7 +635,13 @@ bool Integrator::LoadScene(const char* a_scenePath, const char* a_sncDir)
     m_normMatrices2.resize(0);
   }
 
-  m_pAccelStruct->CommitScene(BUILD_HIGH, BuildOptions::MOTION_BLUR); // to enable more anync may call CommitScene later, but need acync API: CommitSceneStart() ... CommitSceneFinish()
+  uint32_t build_options = BUILD_HIGH;
+  if(m_actualFeatures[Integrator::KSPEC_MOTION_BLUR] == 1)
+  {
+    build_options |= MOTION_BLUR;
+  }
+
+  m_pAccelStruct->CommitScene(build_options); // to enable more anync may call CommitScene later, but need acync API: CommitSceneStart() ... CommitSceneFinish()
   
   // (4) load remap lists and put all of the to the flat data structure
   // 
