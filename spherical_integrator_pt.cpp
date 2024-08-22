d4d86d81e070688d9ec0bf2c633ce3905b5113f7
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2024-08-22 08:29:05 +0300

    Add a spherical integrator
    It can map a hemisphere or a whole sphere (as two hemispheres) into
    a texture and integrate the scene over it.

---
+++ spherical_integrator_pt.cpp
@@ -0,0 +1,63 @@
+#include "spherical_integrator_pt.h"
+
+Integrator::EyeRayData SphericalIntegrator::SampleCameraRay(RandomGen* pGen, uint tid)
+{
+  const uint XY = m_packedXY[tid];
+  const uint x  = (XY & 0x0000FFFF);
+  const uint y  = (XY & 0xFFFF0000) >> 16;
+
+  const float4 pixelOffsets = GetRandomNumbersLens(tid, pGen);
+
+  const float fx = float(x) + pixelOffsets.x;
+  const float fy = float(y) + pixelOffsets.y;
+
+  float xCoordNormalized = (fx + float(m_winStartX))/float(m_fbWidth);
+  const float yCoordNormalized = (fy + float(m_winStartY))/float(m_fbHeight);
+
+  bool isBackside = false;
+  if (m_mode == Mode::Sphere)
+  {
+    if (xCoordNormalized > 0.5f)
+    {
+      xCoordNormalized -= 0.5f;
+      isBackside = true;
+    }
+    xCoordNormalized *= 2.0f;
+  }
+
+  float xWeight = xCoordNormalized * 2.0f - 1.0f;
+  float yWeight = yCoordNormalized * 2.0f - 1.0f;
+
+  float3 forward = to_float3(float4(0, 0, isBackside ? 1.0f : -1.0f, 0));
+  float3 right   = to_float3(float4(1, 0, 0, 0));
+  float3 up      = to_float3(float4(0, 1, 0, 0));
+  const bool validPixel = xWeight * xWeight + yWeight * yWeight < 1.0f;
+
+  float3 rayDir = float3(0, -1, 0);
+  float3 rayPos = float3(0,-10000000,0);
+  if (validPixel)
+  {
+    const float zWeight = std::sqrt(1.0f - xWeight * xWeight - yWeight * yWeight);
+    rayDir = normalize(forward * zWeight + right * xWeight + up * yWeight);
+    rayPos = float3(0, 0, 0);
+  }
+
+  EyeRayData res;
+  {
+    res.rayPos = rayPos;
+    res.rayDir = rayDir;
+    res.x      = x;
+    res.y      = y;
+    res.timeSam = 0.0f;
+    res.waveSam = 1.0f;
+    if(m_normMatrices2.size() != 0)
+      res.timeSam = GetRandomNumbersTime(tid, pGen);
+    if(KSPEC_SPECTRAL_RENDERING !=0 && m_spectral_mode != 0)
+      res.waveSam = GetRandomNumbersSpec(tid, pGen);
+    res.cosTheta = 1.0f;
+  }
+
+  RecordPixelRndIfNeeded(pixelOffsets, float2(res.waveSam, res.timeSam));
+
+  return res;
+}
---
+++ spherical_integrator_pt.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "integrator_pt.h"
+
+class SphericalIntegrator : public Integrator
+{
+public:
+  enum class Mode
+  {
+    Hemisphere,
+    Sphere,
+  };
+
+  SphericalIntegrator(int a_maxThreads = 1, std::vector<uint32_t> a_features = {}, Mode mode = Mode::Hemisphere) :
+    Integrator(a_maxThreads, a_features),
+    m_mode(mode)
+    {}
+  EyeRayData SampleCameraRay(RandomGen* pGen, uint tid) override;
+
+  Mode m_mode = Mode::Hemisphere;
+};
