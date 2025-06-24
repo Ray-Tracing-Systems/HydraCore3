ae5a7a07585e7b560aae7c5e23fb319a34477b0c
Egor-eth <52208679+Egor-eth@users.noreply.github.com>
2025-06-24 18:28:43 +0300

    [WIP] SpecN

--- integrator_pt_f.cpp
+++ integrator_pt_f.cpp
@@ -2,6 +2,8 @@
 #include "include/crandom.h"
 #include "fourier/fspec.h"
 
+#include "specn.h"
+
 #include <chrono>
 #include <string>
 
@@ -394,4 +396,275 @@ void Integrator::kernel_ContributeToBufferF(uint tid, const uint* rayFlags, cons
   for(int i = 0; i < FourierSpec::SIZE; i++) {
     buffer[i] += accumCoeff[i];
   }
+}
+
+void Integrator::PathTraceN(uint tid, uint channels, float* out_color)
+{
+  SpecN accumColor, accumThroughput;
+  float4 rayPosAndNear, rayDirAndFar;
+  SpecN wavelengths;
+  RandomGen gen; 
+  MisData   mis;
+  uint      rayFlags;
+  float     time;
+  kernel_InitEyeRay2(tid, &rayPosAndNear, &rayDirAndFar, &wavelengths, &accumColor, &accumThroughput, &gen, &rayFlags, &mis, &time);
+
+  for(uint depth = 0; depth < m_traceDepth; depth++) 
+  {
+    SpecN shadeColor;
+    float4 hitPart1, hitPart2, hitPart3;
+    uint instId;
+    kernel_RayTrace2(tid, depth, &rayPosAndNear, &rayDirAndFar, &time, 
+                     &hitPart1, &hitPart2, &hitPart3, &instId, &rayFlags);
+    if(isDeadRay(rayFlags))
+      break;
+    
+    kernel_SampleLightSource(tid, &rayPosAndNear, &rayDirAndFar, &wavelengths, &hitPart1, &hitPart2, &hitPart3, &rayFlags, &time,
+                             depth, &gen, &shadeColor);
+
+    kernel_NextBounce(tid, depth, &hitPart1, &hitPart2, &hitPart3, &instId, &shadeColor,
+                      &rayPosAndNear, &rayDirAndFar, &wavelengths, &accumColor, &accumThroughput, &gen, &mis, &rayFlags);
+
+    if(isDeadRay(rayFlags))
+      break;
+  }
+
+  kernel_HitEnvironment(tid, &rayFlags, &rayDirAndFar, &mis, &accumThroughput,
+                        &accumColor);
+
+  kernel_ContributeToImage(tid, &rayFlags, channels, &accumColor, &gen, m_packedXY.data(), &wavelengths, out_color);
+}
+
+void Integrator::kernel_InitEyeRay2N(uint tid, float4* rayPosAndNear, float4* rayDirAndFar, SpecN* wavelengths, 
+                                    SpecN* accumColor,    SpecN* accumuThoroughput,
+                                    RandomGen* gen, uint* rayFlags, MisData* misData, float* time) // 
+{
+
+  if(tid >= m_maxThreadId)
+    return;
+
+  *accumColor        = SpecN(0);
+  *accumuThoroughput = SpecN(1);
+  RandomGen genLocal = m_randomGens[RandomGenId(tid)];
+  *rayFlags          = 0;
+  *misData           = makeInitialMisData();
+
+  EyeRayData r = SampleCameraRay(&genLocal, tid);
+  
+  *wavelengths = SampleWavelengthsN(r.waveSam, LAMBDA_MIN, LAMBDA_MAX);
+
+  *time = r.timeSam;
+ 
+  transform_ray3f(m_worldViewInv, &r.rayPos, &r.rayDir);
+
+  *rayPosAndNear = to_float4(r.rayPos, 0.0f);
+  *rayDirAndFar  = to_float4(r.rayDir, FLT_MAX);
+  *gen           = genLocal;
+}
+
+void Integrator::kernel_SampleLightSourceN(uint tid, const float4* rayPosAndNear, const float4* rayDirAndFar, const SpecN* wavelengths,
+                                          const float4* in_hitPart1, const float4* in_hitPart2, const float4* in_hitPart3,
+                                          const uint* rayFlags, const float* a_time, uint bounce, RandomGen* a_gen, SpecN* out_shadeColor)
+{
+  if(tid >= m_maxThreadId)
+    return;
+  const uint currRayFlags = *rayFlags;
+  if(isDeadRay(currRayFlags))
+    return;
+    
+  const uint32_t matId = extractMatId(currRayFlags);
+  const float3 ray_dir = to_float3(*rayDirAndFar);
+  
+  const float4 data1  = *in_hitPart1;
+  const float4 data2  = *in_hitPart2;
+  const SpecN lambda = *wavelengths;
+
+  SurfaceHit hit;
+  hit.pos  = to_float3(data1);
+  hit.norm = to_float3(data2);
+  hit.tang = to_float3(*in_hitPart3);
+  hit.uv   = float2(data1.w, data2.w);
+  
+  const int bounceTmp = int(bounce); 
+  const float4 rands = GetRandomNumbersLgts(tid, a_gen, bounceTmp); 
+  const int lightId  = std::min(int(std::floor(rands.w * float(m_lights.size()))), int(m_lights.size() - 1u));
+  RecordLightRndIfNeeded(bounce, rands); 
+
+  if(lightId < 0) // no lights or invalid light id
+  {
+    *out_shadeColor = SpecN(0.0f);
+    return;
+  }
+  
+  const LightSample lSam = LightSampleRev(lightId, to_float3(rands), hit.pos);
+  const float  hitDist   = std::sqrt(dot(hit.pos - lSam.pos, hit.pos - lSam.pos));
+
+  const float3 shadowRayDir = normalize(lSam.pos - hit.pos); // explicitSam.direction;
+  const float3 shadowRayPos = hit.pos + hit.norm * std::max(maxcomp(hit.pos), 1.0f)*5e-6f; // TODO: see Ray Tracing Gems, also use flatNormal for offset
+
+  float time = *a_time;
+  const bool   inIllumArea  = (dot(shadowRayDir, lSam.norm) < 0.0f) || lSam.isOmni || lSam.hasIES;
+  const bool   needShade    = inIllumArea && !m_pAccelStruct->RayQuery_AnyHitMotion(to_float4(shadowRayPos, 0.0f), to_float4(shadowRayDir, hitDist*0.9995f), time); /// (!!!) expression-way, RT pipeline bug work around, if change check test_213
+  RecordShadowHitIfNeeded(bounce, needShade);
+
+  if(needShade) /// (!!!) expression-way to compute 'needShade', RT pipeline bug work around, if change check test_213
+  {
+    const BsdfEvalN bsdfV    = MaterialEval(matId, lambda, shadowRayDir, (-1.0f)*ray_dir, hit.norm, hit.tang, hit.uv);
+    float cosThetaOut       = std::max(dot(shadowRayDir, hit.norm), 0.0f);
+    
+    float      lgtPdfW      = LightPdfSelectRev(lightId) * LightEvalPDF(lightId, shadowRayPos, shadowRayDir, lSam.pos, lSam.norm, lSam.pdf);
+    float      misWeight    = (m_intergatorType == INTEGRATOR_MIS_PT) ? misWeightHeuristic(lgtPdfW, bsdfV.pdf) : 1.0f;
+    const bool isDirect     = (m_lights[lightId].geomType == LIGHT_GEOM_DIRECT); 
+    const bool isPoint      = (m_lights[lightId].geomType == LIGHT_GEOM_POINT); 
+    
+    if(isDirect)
+    {
+      misWeight = 1.0f;
+      lgtPdfW   = 1.0f;
+    }
+    else if(isPoint)
+      misWeight = 1.0f;
+
+    const bool isDirectLight = !hasNonSpecular(currRayFlags);
+    if((m_renderLayer == FB_DIRECT   && !isDirectLight) || 
+       (m_renderLayer == FB_INDIRECT && isDirectLight)) // skip some number of bounces if this is set
+      misWeight = 0.0f;
+      
+    
+    const SpecN lightColor = LightIntensity(lightId, lambda, shadowRayPos, shadowRayDir);
+    *out_shadeColor = (lightColor * bsdfV.val / lgtPdfW) * cosThetaOut * misWeight;
+  }
+  else
+    *out_shadeColor = SpecN(0.0f);
+}
+
+SpecN Integrator::LightIntensityN(uint a_lightId, SpecN &a_wavelengths, float3 a_rayPos, float3 a_rayDir)
+{
+  SpecN lightColor;
+  
+  // get spectral data for light source
+  //
+  const uint specId = m_lights[a_lightId].specId;
+
+  const uint2 data  = m_spec_offset_sz[specId];
+  const uint offset = data.x;
+  const uint size   = data.y;
+  lightColor = SampleUniformSpectrum(m_spec_values.data() + offset, a_wavelengths, size);
+
+  lightColor *= m_lights[a_lightId].mult;
+
+  return lightColor;
+}
+
+
+void Integrator::kernel_NextBounceN(uint tid, uint bounce, const float4* in_hitPart1, const float4* in_hitPart2, const float4* in_hitPart3,
+                                   const uint* in_instId, const SpecN* in_shadeColor, float4* rayPosAndNear, float4* rayDirAndFar,
+                                   const SpecN* wavelengths, SpecN* accumColor, SpecN* accumThoroughput,
+                                   RandomGen* a_gen, MisData* misPrev, uint* rayFlags)
+{
+  if(tid >= m_maxThreadId)
+    return;
+  const uint currRayFlags = *rayFlags;
+  if(isDeadRay(currRayFlags))
+    return;
+    
+  const uint32_t matId = extractMatId(currRayFlags);
+
+  // process surface hit case
+  //
+  const float3 ray_dir = to_float3(*rayDirAndFar);
+  const float3 ray_pos = to_float3(*rayPosAndNear);
+  const SpecN lambda  = *wavelengths;
+  
+  const float4 data1 = *in_hitPart1;
+  const float4 data2 = *in_hitPart2;
+  
+  SurfaceHit hit;
+  hit.pos  = to_float3(data1);
+  hit.norm = to_float3(data2);
+  hit.tang = to_float3(*in_hitPart3);
+  hit.uv   = float2(data1.w, data2.w);
+
+  const float hitDist = in_hitPart3->w;
+  
+  const MisData prevBounce = *misPrev;
+  const float   prevPdfW   = prevBounce.matSamplePdf;
+
+  // process light hit case
+  //
+  if(m_materials[matId].mtype == MAT_TYPE_LIGHT_SOURCE)
+  {
+    if(lightId != 0xFFFFFFFF)
+    {
+      const float lightCos = dot(to_float3(*rayDirAndFar), to_float3(m_lights[lightId].norm));
+      const float lightDirectionAtten = (lightCos < 0.0f || m_lights[lightId].geomType == LIGHT_GEOM_SPHERE) ? 1.0f : 0.0f;
+      lightIntensity = LightIntensityN(lightId, lambda, ray_pos, to_float3(*rayDirAndFar))*lightDirectionAtten;
+    }
+
+    float misWeight = 1.0f;
+    if(m_intergatorType == INTEGRATOR_MIS_PT) 
+    {
+      if(bounce > 0 && lightId != 0xFFFFFFFF)
+      {
+        const float lgtPdf  = LightPdfSelectRev(lightId) * LightEvalPDF(lightId, ray_pos, ray_dir, hit.pos, hit.norm, 1.0f);
+        misWeight           = misWeightHeuristic(prevPdfW, lgtPdf);
+        if (prevPdfW <= 0.0f) // specular bounce
+          misWeight = 1.0f;
+      }
+    }
+    else if(m_intergatorType == INTEGRATOR_SHADOW_PT && hasNonSpecular(currRayFlags))
+      misWeight = 0.0f;
+    
+    const bool isDirectLight  = !hasNonSpecular(currRayFlags);
+    const bool isFirstNonSpec = (currRayFlags & RAY_FLAG_FIRST_NON_SPEC) != 0;
+    if(m_renderLayer == FB_INDIRECT && (isDirectLight || isFirstNonSpec))
+      misWeight = 0.0f;
+
+    SpecN currAccumColor      = *accumColor;
+    SpecN currAccumThroughput = *accumThoroughput;
+    
+    currAccumColor += currAccumThroughput * lightIntensity * misWeight;
+   
+    *accumColor = currAccumColor;
+    *rayFlags   = currRayFlags | (RAY_FLAG_IS_DEAD | RAY_FLAG_HIT_LIGHT);
+    return;
+  }
+  
+  const uint bounceTmp    = bounce;
+  const BsdfSampleN matSam = MaterialSampleAndEvalN(matId, tid, bounceTmp, lambda, a_gen, (-1.0f)*ray_dir, hit.norm, hit.tang, hit.uv, misPrev, currRayFlags);
+  const SpecN bxdfVal    = matSam.val * (1.0f / std::max(matSam.pdf, 1e-20f));
+  const float  cosTheta   = std::abs(dot(matSam.dir, hit.norm)); 
+
+  MisData nextBounceData      = *misPrev;        // remember current pdfW for next bounce
+  nextBounceData.matSamplePdf = (matSam.flags & RAY_EVENT_S) != 0 ? -1.0f : matSam.pdf; 
+  nextBounceData.cosTheta     = cosTheta;   
+  *misPrev                    = nextBounceData;
+
+
+  if(m_intergatorType == INTEGRATOR_SHADOW_PT || m_intergatorType == INTEGRATOR_MIS_PT)
+  {
+    const SpecN currThoroughput = *accumThoroughput;
+    const SpecN shadeColor      = *in_shadeColor;
+    SpecN currAccumColor        = *accumColor;
+
+    currAccumColor += currThoroughput * shadeColor;
+    *accumColor       = currAccumColor;
+    *accumThoroughput = currThoroughput*cosTheta*bxdfVal; 
+  }
+
+  // compute point on the other side of the surface in case of transmission
+  if((matSam.flags & RAY_EVENT_T) != 0)
+  {
+    hit.pos = hit.pos + hitDist * ray_dir * 2 * 1e-6f;
+  }  
+
+  *rayPosAndNear = to_float4(OffsRayPos(hit.pos, hit.norm, matSam.dir), 0.0f); // todo: use flatNormal for offset
+  *rayDirAndFar  = to_float4(matSam.dir, FLT_MAX);
+  
+  uint nextFlags = ((currRayFlags & ~RAY_FLAG_FIRST_NON_SPEC) | matSam.flags); // always force reset RAY_FLAG_FIRST_NON_SPEC;
+  if(m_renderLayer == FB_DIRECT && hasNonSpecular(currRayFlags))   // NOTE: use currRayFlags for check, not nextFlags because of MIS: a ray may hit light source in next bounce
+    nextFlags |= RAY_FLAG_IS_DEAD;                                 //       but if we already have non specular bounce previously, definitely can stop  
+  else if(!hasNonSpecular(currRayFlags) && hasNonSpecular(nextFlags))
+    nextFlags |= RAY_FLAG_FIRST_NON_SPEC;
+  *rayFlags      = nextFlags;                                   
 }
---
+++ specn.h
@@ -0,0 +1,205 @@
+#ifndef SPECN_H_
+#define SPECN_H_
+#include <algorithm>
+#include <numeric>
+
+
+
+
+struct SpecN
+{
+    static constexpr int SIZE = 10;
+ 
+    float v[SIZE];
+
+    SpecN() : v{} 
+    {
+        for(int i = 0; i < SIZE; ++i) {
+            v[i] = 0.0f;
+        }
+    }
+
+    explicit SpecN(const float *ptr) : v{} 
+    { 
+        for(int i = 0; i < SIZE; ++i) {
+            v[i] = ptr[i];
+        }
+    }
+
+    SpecN(const SpecN &other) = default;
+
+    explicit SpecN(float x) : v{} {
+        for(int i = 0; i < SIZE; ++i) {
+            v[i] = x;
+        }
+    }
+
+    SpecN &operator=(const SpecN &other) = default;
+
+    SpecN operator+(const SpecN &other) const 
+    {
+        SpecN c(*this);
+        c += other;
+        return c;
+    }
+
+    SpecN operator-(const SpecN &other) const 
+    {
+        SpecN c(*this);
+        c -= other;
+        return c;
+    }
+
+    SpecN operator*(float f) const 
+    {
+        SpecN c(*this);
+        c *= f;
+        return c;
+    }
+
+    SpecN operator/(float f) const 
+    {
+        SpecN c(*this);
+        c /= f;
+        return c;
+    }
+    //Convolution in time domain
+    SpecN operator*(const SpecN &other) const
+    {
+        SpecN c(*this);
+        c *= other;
+        return c;
+    }
+
+    float &operator[](int i) 
+    {
+        return v[i];
+    }
+
+    float operator[](int i) const 
+    {
+        return v[i];
+    }
+
+    SpecN operator-() const 
+    {
+        SpecN res;
+        for(uint i = 0; i < SIZE; ++i) {
+            res.v[i] = -v[i];
+        }
+        return res;
+    }
+
+    SpecN &operator+=(const SpecN &other) 
+    {
+        for(uint i = 0; i < SIZE; ++i) {
+            v[i] += other.v[i];
+        }
+        return *this;
+    }
+
+    SpecN &operator-=(const SpecN &other) 
+    {
+        for(uint i = 0; i < SIZE; ++i) {
+            v[i] -= other.v[i];
+        }
+        return *this;
+    }
+
+    SpecN &operator*=(float f) 
+    {
+        for(uint i = 0; i < SIZE; ++i) {
+            v[i] *= f;
+        }
+        return *this;
+    }
+
+    SpecN &operator/=(float f) 
+    {
+        for(uint i = 0; i < SIZE; ++i) {
+            v[i] /= f;
+        }
+        return *this;
+    }
+
+    SpecN &operator*=(const SpecN &other) 
+    {
+        for(uint i = 0; i < SIZE; ++i) {
+            v[i] *= other.v[i];
+        }
+        return *this;
+    }
+
+    bool operator==(const SpecN &other) const 
+    {
+        for(int i = 0; i < SIZE; ++i) {
+            if(v[i] != other.v[i]) return false;
+        }
+        return true;
+    }
+
+    bool operator!=(const SpecN &other) const 
+    {
+        for(int i = 0; i < SIZE; ++i) {
+            if(v[i] != other.v[i]) return true;
+        }
+        return false;
+    }
+
+
+};
+
+inline SpecN operator*(float f, const SpecN &spec)
+{
+    return spec * f;
+}
+
+
+struct BsdfEvalN
+{
+  SpecN val;
+  float  pdf; 
+};
+
+struct BsdfSampleN
+{
+  SpecN val;
+  float3 dir;
+  float  pdf; 
+  uint   flags;
+  float  ior;
+};
+
+static inline SpecN SampleWavelengthsN(float u, float a, float b) 
+{
+  // pdf is 1.0f / (b - a)
+  SpecN res;
+
+  res[0] = lerp(a, b, u);
+
+  float delta = (b - a) / float(SpecN::SIZE);
+  for (uint32_t i = 1; i < SpecN::SIZE; ++i) 
+  {
+      res[i] = res[i - 1] + delta;
+      if (res[i] > b)
+        res[i] = a + (res[i] - b);
+  }
+
+  return res;
+}
+
+static inline SpecN SampleUniformSpectrumN(const float* a_spec_values, SpecN a_wavelengths, uint32_t a_sz)
+{
+  const int  WAVESN = int(LAMBDA_MAX-LAMBDA_MIN);
+
+  SpecN res;
+  for(int i = 0; i < SpecN::SIZE; ++i) {
+    size_t idx = size_t(a_wavelengths[i] - LAMBDA_MIN);
+    res[i] = a_spec_values[idx];
+  }
+
+  return res;
+}
+
+
+#endif
