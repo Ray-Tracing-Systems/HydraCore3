d390886cbb0dfb6a91e61216eb35fd3abb741d9a
vova@frolov.pp.ru <vova@frolov.pp.ru>
2025-07-26 23:35:18 +0300

    edit

--- testing/run_bench.py
+++ testing/run_bench.py
@@ -1,4 +1,4 @@
-import os, sys, subprocess, re
+import os, sys, subprocess, re, csv
 
 def list_scenes(directory):
   return [(entry, directory + "/" + entry + "/statex_00001.xml") for entry in os.listdir(directory) if os.path.isdir(os.path.join(directory, entry))]
@@ -20,7 +20,8 @@ def run(test_name, args, time_list):
 if __name__ == '__main__':
 
   HYDRA3_PATH = ""
-  SPP = 256
+  SPP = 1024
+  SQRRES = True
 
   os.chdir('..') # use HydraCore3 root dir as current
   if sys.platform == 'win32':
@@ -32,7 +33,21 @@ if __name__ == '__main__':
   scenes = list_scenes("../HydraScenes")
   for scn in scenes:
       args = [HYDRA3_PATH, "-in", scn[1], "-out",  "y_" + scn[0] + ".png", "-spp", str(SPP), "--gpu"]
+      if(SQRRES):
+        args = args + ["-width", "1024", "-height", "1024"]
       print(args)  
       run(scn[0], args, time_list)
   
+  with open('testing/data.csv', 'a', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    
+    # Если файл пустой, добавляем заголовки
+    if file.tell() == 0:
+        writer.writerow(["Scene", "Time"])  # Заголовки столбцов
+    
+    # Записываем данные построчно
+    for name, value in zip(scenes, time_list):
+        suffix = "(1024)" if SQRRES else ""
+        writer.writerow([name[0] + suffix, value])
+
   print(time_list)
