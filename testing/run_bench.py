6e518cc5d91fd365983e6edf3543ead7e4d5f418
vova@frolov.pp.ru <vova@frolov.pp.ru>
2025-08-03 12:54:23 +0300

    edit

--- testing/run_bench.py
+++ testing/run_bench.py
@@ -1,7 +1,13 @@
 import os, sys, subprocess, re, csv
 
+#def list_scenes(directory):
+#  return [(entry, directory + "/" + entry + "/statex_00001.xml") for entry in os.listdir(directory) if os.path.isdir(os.path.join(directory, entry))]
+
 def list_scenes(directory):
-  return [(entry, directory + "/" + entry + "/statex_00001.xml") for entry in os.listdir(directory) if os.path.isdir(os.path.join(directory, entry))]
+    entries = [(entry, directory + "/" + entry + "/statex_00001.xml") 
+              for entry in os.listdir(directory) 
+              if os.path.isdir(os.path.join(directory, entry))]
+    return sorted(entries, key=lambda x: x[0])
 
 def run(test_name, args, time_list):
   try:
@@ -29,25 +35,26 @@ if __name__ == '__main__':
   else:  # if sys.platform == 'linux':
     HYDRA3_PATH = "./bin-release/hydra"
   
-  time_list = []
-  scenes = list_scenes("../HydraScenes")
-  for scn in scenes:
-      args = [HYDRA3_PATH, "-in", scn[1], "-out",  "y_" + scn[0] + ".png", "-spp", str(SPP), "--gpu"]
-      if(SQRRES):
-        args = args + ["-width", "1024", "-height", "1024"]
-      print(args)  
-      run(scn[0], args, time_list)
-  
-  with open('testing/data.csv', 'a', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
+  for SQRRES in [False,True]:
+    time_list = []
+    scenes = list_scenes("../HydraScenes")
+    for scn in scenes:
+        args = [HYDRA3_PATH, "-in", scn[1], "-out",  "y_" + scn[0] + ".png", "-spp", str(SPP), "--gpu"]
+        if(SQRRES):
+          args = args + ["-width", "1024", "-height", "1024"]
+        print(args)  
+        run(scn[0], args, time_list)
     
-    # Если файл пустой, добавляем заголовки
-    if file.tell() == 0:
-        writer.writerow(["Scene", "Time"])  # Заголовки столбцов
-    
-    # Записываем данные построчно
-    for name, value in zip(scenes, time_list):
-        suffix = "(1024)" if SQRRES else ""
-        writer.writerow([name[0] + suffix, value])
+    with open('testing/data.csv', 'a', newline='', encoding='utf-8') as file:
+      writer = csv.writer(file)
+      
+      # Если файл пустой, добавляем заголовки
+      if file.tell() == 0:
+          writer.writerow(["Scene", "Time"])  # Заголовки столбцов
+      
+      # Записываем данные построчно
+      for name, value in zip(scenes, time_list):
+          suffix = "(1024)" if SQRRES else ""
+          writer.writerow([name[0] + suffix, value])
 
-  print(time_list)
+    print(time_list)
